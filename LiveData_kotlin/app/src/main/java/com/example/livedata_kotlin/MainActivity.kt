fb80d0ec12e155ad012b16e182a40c39d628810c
MinChan Youn <dbsalscks0@naver.com>
2021-10-27 16:28:43 +0900

    LiveData study

--- LiveData_kotlin/app/src/main/java/com/example/livedata_kotlin/MainActivity.kt
+++ LiveData_kotlin/app/src/main/java/com/example/livedata_kotlin/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.livedata_kotlin
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.livedata_kotlin.databinding.ActivityMainBinding
 import java.util.*
@@ -12,14 +15,19 @@ class MainActivity : AppCompatActivity() {
     //binding
     private lateinit var binding: ActivityMainBinding
     private lateinit var mModel: RandomNumberViewModel
+    private lateinit var mModel2: TimerViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
+        //TODO: 예제1
         //Get the view model
-        mModel = ViewModelProviders.of(this).get(RandomNumberViewModel::class.java)
+//        mModel = ViewModelProviders.of(this).get(RandomNumberViewModel::class.java) //방법1
+        mModel = ViewModelProvider(this).get(RandomNumberViewModel::class.java) //방법2
 
         //Create the observer which updates the ui
         val randomNumberObserver = Observer<Int> { newNumber ->
@@ -34,6 +42,37 @@ class MainActivity : AppCompatActivity() {
         binding.mButton.setOnClickListener {
             //Change the data
             mModel.currentRandomNUmber.value = Random().nextInt(50)
+        }
+
+
+
+        //TODO: 예제2
+        //Get the view model
+        mModel2 = ViewModelProvider(this).get(TimerViewModel::class.java)
+
+        //Observe case1
+        mModel2.seconds().observe(this, Observer {
+            binding.mTextview2.text = it.toString()
+        })
+
+        //Observe case2
+        mModel2.finished.observe(this, Observer {
+            if(it){
+                Toast.makeText(this, "Finished!", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        binding.mTimeStart.setOnClickListener {
+            if(binding.mInputText2.text.isEmpty() || binding.mInputText2.text.length>10){
+                Toast.makeText(this, "Invaild Number!", Toast.LENGTH_SHORT).show()
+            }else{
+                mModel2.timerValue.value = binding.mInputText2.text.toString().toLong()
+                mModel2.startTimer()
+            }
+        }
+        binding.mTimeFinish.setOnClickListener {
+            binding.mTextview2.text = "0"
+            mModel2.stopTimer()
         }
     }
 }
---
+++ LiveData_kotlin/app/src/main/java/com/example/livedata_kotlin/TimerViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.livedata_kotlin
+
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TimerViewModel : ViewModel() {
+    private lateinit var timer: CountDownTimer
+    var timerValue = MutableLiveData<Long>()
+
+    private val _seconds = MutableLiveData<Int>() //Observe
+    var finished = MutableLiveData<Boolean>() //Observe
+
+    fun seconds(): LiveData<Int>{
+        return _seconds
+    }
+
+    fun startTimer(){
+        timer = object: CountDownTimer(timerValue.value!!.toLong(), 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished/1000
+                _seconds.value = timeLeft.toInt()
+            }
+
+            override fun onFinish() {
+                finished.value = true
+            }
+        }.start()
+    }
+
+    fun stopTimer(){
+        timer.cancel()
+    }
+}
