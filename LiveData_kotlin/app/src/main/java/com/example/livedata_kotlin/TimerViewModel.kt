6720643c3154f3e841f55344b78eb6a9ceaaaf07
MinChan Youn <dbsalscks0@naver.com>
2021-10-28 12:41:54 +0900

    update livedata study

--- LiveData_kotlin/app/src/main/java/com/example/livedata_kotlin/TimerViewModel.kt
+++ LiveData_kotlin/app/src/main/java/com/example/livedata_kotlin/TimerViewModel.kt
@@ -17,6 +17,12 @@ class TimerViewModel : ViewModel() {
         return _seconds
     }
 
+    //이렇게 구문을 사용하는 이유는
+    //실제 _seconds를 직접접근하는것이 아니라 읽기만 가능한 val의 seconds를 접근하여 원본이 수정되지 않도록 하기 위해서!
+    val seconds: LiveData<Int>
+        get() = _seconds
+
+
     fun startTimer(){
         timer = object: CountDownTimer(timerValue.value!!.toLong(), 1000){
             override fun onTick(millisUntilFinished: Long) {
