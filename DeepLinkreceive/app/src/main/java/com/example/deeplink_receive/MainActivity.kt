2eb4b2f609a37690dec857a9226e250aeba6a618
younminchan <dbsalscks0@naver.com>
2022-02-22 21:45:36 +0900

    DeepLink 수신 코드 작성

--- DeepLinkreceive/app/src/main/java/com/example/deeplink_receive/MainActivity.kt
+++ DeepLinkreceive/app/src/main/java/com/example/deeplink_receive/MainActivity.kt
@@ -12,5 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initDeepLink()
+    }
+
+    /** DeepLink */
+    private fun initDeepLink() {
+        if (Intent.ACTION_VIEW.equals(intent.action)) {
+            var uri = intent.data
+            if (uri != null) {
+                //TODO: "deeplink://host?data1=DeepLinkData1&data2=DeepLinkData2"
+                var dl_data1 = uri.getQueryParameter("data1")
+                var dl_data2 = uri.getQueryParameter("data2")
+
+                binding.tvDeeplinkReceive.text = "딥링크 수신받은 값\n" +
+                        "dl_data1: $dl_data1 \n" +
+                        "dl_data2: $dl_data2"
+            }
+        }
     }
 }
