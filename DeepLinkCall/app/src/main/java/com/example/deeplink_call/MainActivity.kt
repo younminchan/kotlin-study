c52f5d52ae94fc56e48f3cac082a97fdb232809a
younminchan <dbsalscks0@naver.com>
2022-02-22 21:36:15 +0900

    DeepLink 호출코드 작성
    DeepLink 호출코드 작성

--- DeepLinkCall/app/src/main/java/com/example/deeplink_call/MainActivity.kt
+++ DeepLinkCall/app/src/main/java/com/example/deeplink_call/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.deeplink_call
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.deeplink_call.databinding.ActivityMainBinding
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        /** ViewBinding 설정 */
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /** Click-Listener */
+        binding.tvDeepLink.setOnClickListener {
+            try {
+                var url = "deeplink://host?data1=DeepLinkData1&data2=DeepLinkData2"
+                var intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK //새로운 창
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                /** 딥링크 호출 오류(해당 앱이 존재하지 않을 경우)*/
+                Toast.makeText(this, "DeepLink 호출이 불가능 합니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
