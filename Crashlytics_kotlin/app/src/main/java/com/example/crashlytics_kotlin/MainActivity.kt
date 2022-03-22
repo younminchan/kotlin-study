7962ecbbed61dcc75dfe058033d1b9263e64844c
younminchan <dbsalscks0@naver.com>
2022-03-22 15:03:13 +0900

    update crashlytics

--- Crashlytics_kotlin/app/src/main/java/com/example/crashlytics_kotlin/MainActivity.kt
+++ Crashlytics_kotlin/app/src/main/java/com/example/crashlytics_kotlin/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.crashlytics_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.crashlytics_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvException.setOnClickListener {
+            throw RuntimeException("Test Crash")
+        }
     }
 }
