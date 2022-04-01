6a3c78b20cb84b09183c8e785d0ecc6f5cda2494
younminchan <dbsalscks0@naver.com>
2022-04-01 09:47:49 +0900

    Update MyFirebaseMessagingService.kt
    잘못된 코드 수정

--- FCMkotlin/app/src/main/java/com/example/fcm_kotlin/MyFirebaseMessagingService.kt
+++ FCMkotlin/app/src/main/java/com/example/fcm_kotlin/MyFirebaseMessagingService.kt
@@ -98,8 +98,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // 알림에 대한 UI 정보, 작업
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.mipmap.ic_launcher)                     // 아이콘 설정
-            .setContentTitle(remoteMessage.data["body"].toString()) // 제목
-            .setContentText(remoteMessage.data["title"].toString()) // 메시지 내용
+            .setContentTitle(remoteMessage.data["title"].toString())// 제목
+            .setContentText(remoteMessage.data["body"].toString())  // 메시지 내용
             .setAutoCancel(true)                                    // 알람클릭시 삭제여부
             .setSound(soundUri)                                     // 알림 소리
             .setContentIntent(resultPendingIntent) //TODO: (방법1)MainActivity -> ResultActivity로 이동
