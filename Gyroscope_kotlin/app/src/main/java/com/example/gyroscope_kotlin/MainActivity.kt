f1ec41908c7c52dd6ef059a3a5afe86fe376bf7a
younminchan <dbsalscks0@naver.com>
2022-04-04 11:18:31 +0900

    Update MainActivity.kt

--- Gyroscope_kotlin/app/src/main/java/com/example/gyroscope_kotlin/MainActivity.kt
+++ Gyroscope_kotlin/app/src/main/java/com/example/gyroscope_kotlin/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             SensorManager.SENSOR_DELAY_NORMAL
         )
         /**
-         * SENSOR_DELAY_FASTEST: 가능한 자주
+         * SENSOR_DELAY_FASTEST: 빈번하게
          * SENSOR_DELAY_GAME: 게임에 적합한 정도
          * SENSOR_DELAY_NOMAL: 화면 방향이 전환될 때 적합한 정도
          * SENSOR_DELAY_DELAY_UI: 사용자 인터페이스를 표시하기에 적합한 정도
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager.unregisterListener(this)
     }
 
-    //센서 정밀도가 변경되면 호출
+    /** 센서 정밀도가 변경되면 호출 */
     override fun onSensorChanged(sensorEvent: SensorEvent?) {
         /**
          * SensorEvent.values[0]: x축
@@ -47,11 +47,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
          * SensorEvent.values[2]: z축
          */
         if (sensorEvent != null) {
-            Log.e("YMC", "(X축) ${sensorEvent.values[0]} / (Y축) ${sensorEvent.values[1]} / (Z축) ${sensorEvent.values[2]}")
+            var sensorResult = "(X축) ${sensorEvent.values[0]} / (Y축) ${sensorEvent.values[1]} / (Z축) ${sensorEvent.values[2]}"
+            binding.tvGyroscope.text = sensorResult
+            Log.e("YMC", sensorResult)
         }
     }
 
-    //센서값이 변경되면 호출
+    /** 센서값이 변경되면 호출 */
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
     }
