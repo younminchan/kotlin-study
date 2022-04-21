7b7d7083811d00e717344ff7068838ab676aa8d0
younminchan <dbsalscks0@naver.com>
2022-04-21 17:14:00 +0900

    update ProductFlavors

--- ProductFlavors_kotlin/app/src/main/java/com/example/productflavors_kotlin/MainActivity.kt
+++ ProductFlavors_kotlin/app/src/main/java/com/example/productflavors_kotlin/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.productflavors_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.productflavors_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
