69f9a830e3cf5c017cb6d64055d6268e50666aef
younminchan <dbsalscks0@naver.com>
2022-07-06 11:16:08 +0900

    update ViewPager2 - TabLayout

--- BottomNavigationViewpager_kotlin/app/src/main/java/com/example/bottomnavigation_viewpager_kotlin/MainActivity.kt
+++ BottomNavigationViewpager_kotlin/app/src/main/java/com/example/bottomnavigation_viewpager_kotlin/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.bottomnavigation_viewpager_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.viewpager2.widget.ViewPager2
 import com.example.bottomnavigation_viewpager_kotlin.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -16,7 +20,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViewPager() {
+        //ViewPager2 Adapter 셋팅
+        var viewPager2Adatper = ViewPager2Adapter(this)
+        viewPager2Adatper.addFragment(Tab1Fragment())
+        viewPager2Adatper.addFragment(Tab2Fragment())
+        viewPager2Adatper.addFragment(Tab3Fragment())
 
+        //Adapter 연결
+        binding.vpViewpagerMain.apply {
+            adapter = viewPager2Adatper
+
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                }
+            })
+        }
+
+        //ViewPager, TabLayout 연결
+        TabLayoutMediator(binding.tlNavigationView, binding.vpViewpagerMain) { tab, position ->
+            Log.e("YMC", "ViewPager position: ${position}")
+            when (position) {
+                0 -> tab.text = "Tab1"
+                1 -> tab.text = "Tab2"
+                2 -> tab.text = "Tab3"
+            }
+        }.attach()
     }
 
 }
---
+++ BottomNavigationViewpager_kotlin/app/src/main/java/com/example/bottomnavigation_viewpager_kotlin/Tab2Fragment.kt
@@ -0,0 +1,24 @@
+package com.example.bottomnavigation_viewpager_kotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bottomnavigation_viewpager_kotlin.databinding.FragmentTab2Binding
+
+class Tab2Fragment : Fragment() {
+    private var _binding: FragmentTab2Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
+        _binding = FragmentTab2Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
